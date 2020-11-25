6e24a380ca7abcd28df20c319eca64739f9b9dbe
dhencios15 <ddelacruz@umindanao.edu.ph>
2020-11-25 23:18:05 +0800

    partial design

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      nerko: ['Nerko One', 'cursive']
+    },
+    extend: {
+      rotate: {
+        '-180': '-180deg',
+        '-90': '-90deg',
+        '-45': '-45deg',
+        '-10': '-10deg',
+        '-9': '-9deg',
+        '-8': '-8deg',
+        '-7': '-7deg',
+        '-6': '-6deg',
+        '-5': '-5deg',
+        '-4': '-4deg',
+        '-3': '-3deg',
+        '-2': '-2deg',
+        '-1': '-1deg',
+        0: '0',
+        1: '1deg',
+        2: '2deg',
+        3: '3deg',
+        4: '4deg',
+        5: '5deg',
+        6: '6deg',
+        7: '7deg',
+        8: '8deg',
+        9: '9deg',
+        10: '10deg',
+        45: '45deg',
+        90: '90deg',
+        180: '180deg'
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
